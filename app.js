5fd59fd6d684307189ff3f437ecc04e27b998841
Frost <frost@domo.com>
2013-08-14 16:55:52 -0600

    initial step 4

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  @cast object will get automatically created by the Chomecast extension
   ONLY IF YOU HAVE THE CORRECT HTML TAG DECLARATIONS
  */
-var cast, cast_api, cv_activity;
+var cast, cast_api, cv_activity, receivers, selectedReceiver, sending;
 
 if (cast && cast.isAvailable) {
   // Cast is known to be available
@@ -24,21 +24,34 @@ function init(){
   // Find all devices that are nearby that are capable of performing "YouTube" activities, AKA: All Nearby Chromecasts
   cast_api.addReceiverListener("YouTube", onReceiverList);
 
+  $('.btn.btn-success').show().on('click', function(){
+    sendSomethingToBloodyReceiver(selectedReceiver);
+  });
+
 }
 
 function onReceiverList(list) {
-  if(!list || !list.length) return;
+  if((receivers && receivers.length) || !list || !list.length) return;
+  receivers = list;
+  $('.receivers').empty();
+  receivers.forEach(function(receiver) {
+    $listItem = $('<li><a data-id="' + receiver.id + '">' + receiver.name + '</a></li>');
+    $listItem.on('click', function(e){
+      var $target = $(e.target);
+      selectedReceiver = _.find(receivers, function(receiver){
+        return receiver.id === $target.data('id');
+      });
+      console.log('selectedReceiver', selectedReceiver);
+      
+    });
+    $('.receivers').append($listItem);
 
-  /*
-   AFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAF
-      Now that we have he receiver, we need to launch something to the Chromecast
-   AFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAFAF
-   */
-  var receiver = list[0];
-  sendSomethingToBloodyReceiver(receiver);
+  });
 }
 
 function sendSomethingToBloodyReceiver(r){
+  if(sending) return;
+  sending = true;
   console.log('Sending Launch Request');
   var request = new cast.LaunchRequest("36575150-be79-4eae-a940-11b0ed0ae78b_1", r);
   request.description = new cast.LaunchDescription();
@@ -53,6 +66,12 @@ function onLaunch(activity){
     cv_activity = activity;
 
     //REMIND ME TO PUT IN A RANDOM WORD HERE
-    cast_api.sendMessage(cv_activity.activityId, 'AF-GDG-GOOGLECAST-DEMO', {type: 'Hello GDG Utah', randomWord:''});
+    cast_api.sendMessage(cv_activity.activityId, 'AF-GDG-GOOGLECAST-DEMO', {
+      type: 'Hello GDG Utah',
+      name: $('#name').val(),
+      email: $('#email').val(),
+      favorite: $('#favorite').val(),
+      randomWord:'Queen is the best band EVAR!!!'
+    });
   }
 }
